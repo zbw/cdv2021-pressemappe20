689a4d327f396a01132b1c0ea77d8b05c1732a38
Joachim Neubert <j.neubert@zbw.eu>
2021-04-20 08:19:19 +0200

    Make generate_cdv_dataset.sh with python3

--- bin/generate_cdv_dataset.sh
+++ bin/generate_cdv_dataset.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # nbt, 31.8.2018
 
+# requires python3.6+ and modules pyld and requests
+#   pip3 install --user pyld
+#   pip3 install --user requests
+
 QUERY_DIR=../sparql
 DATA_DIR=../data
 ENDPOINT=http://zbw.eu/beta/sparql/pm20/query
-ZIP=_build/cdv202121_pressemappe20.zip
+ZIP=_build/cdv2021_pressemappe20.zip
 
 # extract the turtle data from the endpoint
 curl --silent  -X POST -H "Content-type: application/sparql-query" -H "Accept: application/ld+json" \
@@ -12,14 +16,15 @@ curl --silent  -X POST -H "Content-type: application/sparql-query" -H "Accept: a
   > $DATA_DIR/pm20_cdv2021.interim.jsonld
   
 # shape jsonld with custom frame and context
-python transform_jsonld.py > $DATA_DIR/pm20_cdv2021.jsonld
+python3 transform_jsonld.py > $DATA_DIR/pm20_cdv2021.jsonld
 
 # extract examples
 cat $DATA_DIR/pm20_cdv2021.jsonld | ./extract_examples.sh > ../examples.jsonld
 
 # combine files in build directory
 cd ..
-/usr/bin/rm -f _build/*
+/usr/bin/rm -rf _build
+mkdir -p _build
 zip -r $ZIP data -x \*~ *.interim.*
 zip -r $ZIP LICENSE.txt
 zip -r $ZIP README.md
